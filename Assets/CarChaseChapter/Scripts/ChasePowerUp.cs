ea4223bbce500a0c581ae8cc12555b6b0d26c4ae
Yavuzhan <74991593+Lethrinus@users.noreply.github.com>
2024-07-29 22:08:20 +0300

    4

--- Assets/CarChaseChapter/Scripts/ChasePowerUp.cs
+++
@@ -1,8 +0,0 @@
-
-using UnityEngine;
-
-public abstract class ChasePowerUp : MonoBehaviour
-{
-    public float duration = 5.0f;
-    public abstract void ApplyEffect(TeddyBearChaseController player);
-}
